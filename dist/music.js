5c67e7f32a1c5a966fb2c329e6e44cc210f4dd89
XXXxxx-collab <2578657473@qq.com>
2022-03-24 19:10:15 +0800

    Site updated: 2022-03-24 19:10:14

--- dist/music.js
+++ dist/music.js
@@ -5,16 +5,16 @@ const ap = new APlayer({
   autoplay: true,
   audio: [
     {
-      name: '哪里都是你',
-      artist: '队长',
-      url: 'http://music.163.com/song/media/outer/url?id=488249475.mp3',
-      cover: 'http://p2.music.126.net/bD6Mc8u993RcXa16-26XsQ==/109951167138581770.jpg?param=177y177',
-    },
-    {
       name: '多远都要在一起',
       artist: 'G.E.M.邓紫棋',
       url: 'http://music.163.com/song/media/outer/url?id=30612793.mp3',
       cover: 'http://p1.music.126.net/kVwk6b8Qdya8oDyGDcyAVA==/1364493930777368.jpg?param=130y130',
+    },
+    {
+      name: '哪里都是你',
+      artist: '队长',
+      url: 'http://music.163.com/song/media/outer/url?id=488249475.mp3',
+      cover: 'http://p2.music.126.net/bD6Mc8u993RcXa16-26XsQ==/109951167138581770.jpg?param=177y177',
     },
     {
       name: '温柔',
