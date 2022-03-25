b02f27ba6238baa829e3502fc6e29975e6bddaaf
XXXxxx-collab <2578657473@qq.com>
2022-03-25 19:07:10 +0800

    Site updated: 2022-03-25 19:07:09

---
+++ dist/music.js
@@ -0,0 +1,51 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  volume: 0.1,
+  autoplay: true,
+  audio: [
+    {
+      name: '多远都要在一起',
+      artist: 'G.E.M.邓紫棋',
+      url: 'http://music.163.com/song/media/outer/url?id=30612793.mp3',
+      cover: 'http://p1.music.126.net/kVwk6b8Qdya8oDyGDcyAVA==/1364493930777368.jpg?param=130y130',
+    },
+    {
+      name: '哪里都是你',
+      artist: '队长',
+      url: 'http://music.163.com/song/media/outer/url?id=488249475.mp3',
+      cover: 'http://p2.music.126.net/bD6Mc8u993RcXa16-26XsQ==/109951167138581770.jpg?param=177y177',
+    },
+    {
+      name: '温柔',
+      artist: '五月天',
+      url: 'http://music.163.com/song/media/outer/url?id=386538.mp3',
+      cover: 'http://p2.music.126.net/v4V40sXKnaqsG0ACyY0aDg==/109951164912221924.jpg?param=130y130',
+    },
+    {
+      name: '我用什么把你留住 (Remix)',
+      artist: '福禄寿FloruitShow',
+      url: 'http://music.163.com/song/media/outer/url?id=1860678388.mp3',
+      cover: 'http://p1.music.126.net/-pS9QZROJ4UTSI1Tg2VSyg==/109951163529550258.jpg?param=130y130',
+    },
+    {
+      name: "虚拟",
+      artist: '陈粒',
+      url: 'http://music.163.com/song/media/outer/url?id=421423808.mp3',
+      cover: 'http://p1.music.126.net/HQxTggMCB7AHUXN-ZFEtmA==/1371091013186741.jpg?param=130y130',
+    },
+    {
+      name: "水星记",
+      artist: '郭顶',
+      url: 'http://music.163.com/song/media/outer/url?id=441491828.mp3',
+      cover: 'http://p2.music.126.net/wSMfGvFzOAYRU_yVIfquAA==/2946691248081599.jpg?param=130y130',
+    },
+    {
+      name: "陪你度过漫长岁月",
+      artist: '陈奕迅',
+      url: 'http://music.163.com/song/media/outer/url?id=35403523.mp3',
+      cover: 'http://p1.music.126.net/Nm5rwMYaH_20Zh61XhGn8Q==/109951166600775221.jpg?param=130y130',
+    },
+  ]
+});
+
